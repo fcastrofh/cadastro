f324023a81a05fa1574da19c9fdc27e31121ab29
Flavio Luciano <fcastrofh@gmail.com>
2020-01-21 16:30:27 -0300

    Add Model Produto

--- src/model/produto.java
+++ src/model/produto.java
@@ -1,5 +1,32 @@
 package model;
 
 public class produto {
+	private double preco;
+	private int desconto;
+	private String tipo;
+	
+	public double getPreco() {
+		return preco;
+	}
+	
+	public void setPreco(double p) {
+		preco = p;
+	}
+	
+	public int getDesconto() {
+		return desconto;
+	}
+	
+	public void setDesconto(int d) {
+		desconto = d;
+	}
+	
+	public String getTipo() {
+		return tipo;
+	}
+	
+	public void setTipo(String t) {
+		tipo = t;
+	}
 
 }
